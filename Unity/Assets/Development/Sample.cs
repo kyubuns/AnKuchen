0bf6399d3978901464cc3ef2a1706e9b17ab3f5f
kyubuns <kyubuns@gmail.com>
2020-08-30 18:42:03 +0900

    chore: sample

--- Unity/Assets/Development/Sample.cs
+++ Unity/Assets/Development/Sample.cs
@@ -11,33 +11,26 @@ namespace AnKuchen.Sample
 
         public void Start()
         {
-            var original = root.GetChild("HogeButton");
-            using (var layouter = Layouter.TopToBottom(original))
-            {
-                foreach (var button in new[] { "Hoge", "Fuga", "Piyo" })
-                {
-                    var newButton = layouter.Create();
-                    newButton.Get<Text>("Text").text = button;
-                }
-            }
+            root.Get("Text").SetActive(false);
         }
     }
 
-public class SampleUIElements
-{
-    public GameObject Root { get; }
-    public Button HogeButton { get; }
-    public Text HogeButtonText { get; }
-    public Button FugaButton { get; }
-    public Text FugaButtonText { get; }
-
-    public SampleUIElements(IMapper mapper)
+    public class UIElements
     {
-        Root = mapper.Get();
-        HogeButton = mapper.Get<Button>("HogeButton");
-        HogeButtonText = mapper.Get<Text>("HogeButton/Text");
-        FugaButton = mapper.Get<Button>("FugaButton");
-        FugaButtonText = mapper.Get<Text>("FugaButton/Text");
+        public GameObject Root { get; }
+        public Button HogeButton { get; }
+        public Text HogeButtonText { get; }
+        public Button FugaButton { get; }
+        public Text FugaButtonText { get; }
+
+        public UIElements(IMapper mapper)
+        {
+            Root = mapper.Get();
+            HogeButton = mapper.Get<Button>("HogeButton");
+            HogeButtonText = mapper.Get<Text>("HogeButton/Text");
+            FugaButton = mapper.Get<Button>("FugaButton");
+            FugaButtonText = mapper.Get<Text>("FugaButton/Text");
+        }
     }
 }
-}
+
